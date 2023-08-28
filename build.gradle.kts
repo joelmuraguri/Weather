58c843e3a4762cf242c6bb9e0d22bfe02e2f1863
joelmuraguri <joelkmuraguri21@gmail.com>
2023-08-28 12:27:54 +0300

    add add model module

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kapt) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
 
 }
 true // Needed to make the Suppress annotation work for the plugins block
---
+++ core/model/src/main/java/com/jk/model/MyClass.kt
@@ -0,0 +1,4 @@
+package com.jk.model
+
+class MyClass {
+}
